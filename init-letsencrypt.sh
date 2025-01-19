bf1ff2cc9d9ed8ee1809ccdd4fef269e9f8db55f
alexeyturalysov <alexeyturalysov@gmail.com>
2025-01-19 09:34:54 +0900

    second

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(docr.smmhub.ru www.docr.smmhub.ru)
+domains=(ugosty.ru www.ugosty.ru)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="alexeyturalysov@gmail.com" # Adding a valid address is strongly recommended
